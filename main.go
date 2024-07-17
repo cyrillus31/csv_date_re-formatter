b82df76efd590ec579c81fe8410ca3a71176c28e
Kirill Fedtsov <kirill.olegovich31@gmail.com>
2024-07-18 00:54:56 +0300

    feat: add reading stdin of needed rows

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	// "fmt"
+
 	"fmt"
+	"time"
 
 	"github.com/cyrillus31/csv_date_re-formatter/utils"
 )
@@ -10,11 +13,15 @@ const INPUT_FOLDER = "input_files/"
 const OUTPUT_FOLDER = "output_files/"
 
 func main() {
+	rowNumber := utils.GetRowNumbers()
+	fmt.Println(rowNumber)
+	time.Sleep(3 * time.Second)
 	files := utils.FindAllInputFiles(INPUT_FOLDER)
 	// fmt.Printf("%v\n", files)
 	for _, file := range files {
-		// utils.PrintFileContent(file)
+		utils.PrintFileContent(file)
 		result, _ := utils.GetFileContent(file)
+		fmt.Println(result)
 	}
 
 }
--- utils/utils.go
+++ utils/utils.go
@@ -10,6 +10,38 @@ import (
 	"strings"
 )
 
+var rowMap = map[rune]int{
+	'a': 0,
+	'b': 1,
+	'c': 2,
+	'd': 4,
+	'e': 5,
+	'f': 6,
+	'g': 7,
+	'h': 8,
+	'i': 9,
+	'j': 10,
+	'k': 11,
+	'l': 12,
+}
+
+func GetRowNumbers() []int {
+	var input string
+	fmt.Print("Введите латинские буквы, соответствующие нужным столбцам:\n")
+	reader := bufio.NewReader(os.Stdin)
+	input, _ = reader.ReadString('\n')
+	return func() []int {
+		var result = []int{}
+		arr := strings.Split(input, " ")
+		// fmt.Println(arr)
+		for _, element := range arr {
+			result = append(result, int(element[0]-'a'))
+		}
+		return result
+	}()
+
+}
+
 func FindAllInputFiles(inputFolder string) []string {
 	files := []string{}
 	filepath.WalkDir(inputFolder, func(path string, d fs.DirEntry, err error) error {
